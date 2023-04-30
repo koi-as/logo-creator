f60643b5cdc26b220868b9e3751067a909ce747b
Koi-AS <lightningblade.901@gmail.com>
2023-04-30 00:29:48 -0400

    Inquirer properly works

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-function init () {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'logoText',
-                message: 'Please choose up to 3 letters for your logo:'
-            },
-            {
-                type: 'input',
-                name: 'textColor',
-                message: 'Please choose a color for your logo:'
-            },
-            {
-                type: 'choice',
-                name: 'shape',
-                message: 'Please choose one of the following shapes for your logo:',
-                choice: ['Square', 'Triangle', 'Circle']
-            },
-            {
-                type: 'input',
-                name: 'color',
-                message: 'Please choose a color for your logo:'
-            }
-        ])
-        .then((data) => {
-            //something goes here
-        })
-};
 
-function writeToFile () {};
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'logoText',
+            message: 'Please choose up to 3 letters for your logo:'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Please choose a color for your logo:'
+        },
+        {
+            type: 'choice',
+            name: 'shape',
+            message: 'Please choose one of the following shapes for your logo:',
+            choice: ['Square', 'Triangle', 'Circle']
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'Please choose a color for your logo:'
+        }
+    ])
+    .then((data) => {
+        const filename = `logo.svg`
 
-init()
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) => {
+            err ? console.log(err) : console.log('success')
+        });
+    });
