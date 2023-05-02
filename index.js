d977de548f660b464be3580f9b39c1fc5a48d69e
Koi-AS <lightningblade.901@gmail.com>
2023-05-02 16:00:15 -0400

    Final changes and touchups. Using logo.html from recommendation over logo.svg

--- index.js
+++ index.js
@@ -54,7 +54,18 @@ inquirer
 const writeToFile = (generateSvg) => {
     const svgFile = generateSvg
 
-    fs.writeFile('./examples/logo.svg', svgFile, (err) => {
+    fs.writeFile('./examples/logo.html', `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        ${svgFile}
+    </body>
+    </html>`, (err) => {
         err ? console.log(error) : console.log('Written to file')
     })
 }
